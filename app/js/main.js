78a2b01785d363f788655a1fc45e0abb8a121171
mikel <mikelgoti@gmail.com>
2023-03-05 14:21:33 +0100

    varios cambios implementados.

--- app/js/main.js
+++ app/js/main.js
@@ -39,3 +39,84 @@ function scrollHeader(){
     }
 }
 window.addEventListener('scroll',scrollHeader)
+
+/**
+ * FLECHA SUBIR ARRIBA AL BAJAR DE CIERTO HEIGHT EN LA PAGINA
+ */
+function desplazarArriba(){
+    const arriba = document.getElementById("flecha_arriba");
+
+    if(this.scrollY >= 350){
+        arriba.classList.add("show-scroll");
+    }
+    else{
+        arriba.classList.remove("show-scroll");
+    }
+}
+window.addEventListener("scroll", desplazarArriba);
+
+/**
+ * MENU HAMBURGER CLICKAR EN CADA SECCION Y MOSTRAR LA PARTE CORRESPONDIENTE
+ */
+const sections = document.querySelectorAll("section[id]");
+
+function desplazamientoActivo() {
+    const scrollY = window.pageYOffset;
+  
+    sections.forEach(current => {
+      const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 58,
+        sectionId = current.getAttribute("id");
+  
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+        const links = [...document.querySelectorAll(`.nav__menu a[href="#${sectionId}"]`)];
+        links.forEach(link => link.classList.add("active-link"));
+      } else {
+        const links = [...document.querySelectorAll(`.nav__menu a[href="#${sectionId}"]`)];
+        links.forEach(link => link.classList.remove("active-link"));
+      }
+    });
+  
+    // Add the active link class to the "INICIO" section link
+    if (scrollY < 200) {
+      const links = [...document.querySelectorAll(`.nav__menu a[href="#inicio"]`)];
+      links.forEach(link => link.classList.add("active-link"));
+    } else {
+      const links = [...document.querySelectorAll(`.nav__menu a[href="#inicio"]`)];
+      links.forEach(link => link.classList.remove("active-link"));
+    }
+  
+    // Add the active link class to the "CONTACTO" section link
+    const lastSection = sections[sections.length - 1];
+    const lastSectionTop = lastSection.offsetTop - 58;
+    const lastSectionHeight = lastSection.offsetHeight;
+    const windowHeight = window.innerHeight;
+  
+    if (scrollY > lastSectionTop - windowHeight / 2 && scrollY <= lastSectionTop + lastSectionHeight) {
+      const links = [...document.querySelectorAll(`.nav__menu a[href="#contacto-general"]`)];
+      links.forEach(link => link.classList.add("active-link"));
+  
+      // Remove the active link class from the "Servicios" link when "Contacto" is active
+      const serviciosLink = document.querySelector('.nav__menu a[href="#servicios"]');
+      serviciosLink.classList.remove('active-link');
+    } else {
+      const links = [...document.querySelectorAll(`.nav__menu a[href="#contacto-general"]`)];
+      links.forEach(link => link.classList.remove("active-link"));
+    }
+  
+    // Add the active link class to the "SERVICIOS" section link
+    const penultimateSection = sections[sections.length - 2];
+    const penultimateSectionTop = penultimateSection.offsetTop - 58;
+    const penultimateSectionHeight = penultimateSection.offsetHeight;
+  
+    if (scrollY > penultimateSectionTop - windowHeight / 2 && scrollY <= penultimateSectionTop + penultimateSectionHeight) {
+      const links = [...document.querySelectorAll(`.nav__menu a[href="#servicios"]`)];
+      links.forEach(link => link.classList.add("active-link"));
+    } else {
+      const links = [...document.querySelectorAll(`.nav__menu a[href="#servicios"]`)];
+      links.forEach(link => link.classList.remove("active-link"));
+    }
+  }
+
+window.addEventListener("scroll", desplazamientoActivo);
+
